4e4019cab30cf35b1ea7423c07d9fc3957610a50
Nithyasri-2516 <nithyasri1625@gmail.com>
2024-10-10 10:58:18 +0530

    init

--- interface.ts
+++ interface.ts
@@ -2,9 +2,7 @@
 // and methods that an object should have, allowing for type-checking
 
 /*  why use interface? 
-   Type Safety: Interfaces provide a way to enforce type checking, reducing runtime errors by ensuring
-    that objects conform to expected structures.
-
+   
 1.Code Readability: They improve code clarity by defining clear contracts for what an object should look like, 
 making it easier for developers to understand and maintain the code.
 
@@ -13,5 +11,8 @@ defined structures, facilitating easier updates and changes.
 
 3.Multiple Implementations: A class can implement multiple interfaces, allowing for flexible designs and promoting
 code reuse.
+
+4.Type Safety: Interfaces provide a way to enforce type checking, reducing runtime errors by ensuring
+that objects conform to expected structures.
 */
 
