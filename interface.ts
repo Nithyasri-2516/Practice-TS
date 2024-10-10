d8cb9293fb39314d1461d298826143db5489e9c2
Nithyasri-2516 <nithyasri1625@gmail.com>
2024-10-10 10:44:29 +0530

    add

---
+++ interface.ts
@@ -0,0 +1,17 @@
+// interface is a syntactical contract that defines the structure of an object. It specifies the properties
+// and methods that an object should have, allowing for type-checking
+
+/*  why use interface? 
+   Type Safety: Interfaces provide a way to enforce type checking, reducing runtime errors by ensuring
+    that objects conform to expected structures.
+
+1.Code Readability: They improve code clarity by defining clear contracts for what an object should look like, 
+making it easier for developers to understand and maintain the code.
+
+2.Modularity: Interfaces promote modular design by allowing different parts of the code to communicate through 
+defined structures, facilitating easier updates and changes.
+
+3.Multiple Implementations: A class can implement multiple interfaces, allowing for flexible designs and promoting
+code reuse.
+*/
+
